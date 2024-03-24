233f416d09b5c929ab402f9499822047f3d17fc8
Ajay Kumar <124868129+ajay-kumar-iphtech@users.noreply.github.com>
2024-03-24 23:07:06 +0530

    scroll one cell at a time fix

--- Yummie/View/OnboardingView/OnboardingViewController.swift
+++ Yummie/View/OnboardingView/OnboardingViewController.swift
@@ -46,6 +46,20 @@ class OnboardingViewController: UIViewController {
         onBoardingCollectionView.dataSource = self
         
         intitialSetUp()
+        if let layout = onBoardingCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+            layout.minimumLineSpacing = 0
+            layout.minimumInteritemSpacing = 0
+            layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+            layout.itemSize = CGSize(width: 393, height: 700)
+        }
+//        if let layout = onBoardingCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+//            layout.scrollDirection = .horizontal
+//            layout.minimumLineSpacing = 10
+//            layout.minimumInteritemSpacing = 0
+//            layout.sectionInset = UIEdgeInsets(top: 0, left: 00, bottom: 0, right: 0)
+//         //   layout.itemSize = CGSize(width: photoCollectionView.frame.size.width / 3 + 10, height: photoCollectionView.frame.size.height)
+//        }
         
     }
     
